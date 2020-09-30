c54572de3003836dd337ada04d8eaeb38885dc96
felipe-carvalho12 <67564586+felipe-carvalho12@users.noreply.github.com>
2020-09-29 23:38:15 -0300

    Added animation to the bird

--- Pipe.py
+++ Pipe.py
@@ -1,7 +1,10 @@
 class Pipe:
-    def __init__(self, surface, center):
+    def __init__(self, surface, midtop=None, midbottom=None):
         self.surface = surface
-        self.rect = self.surface.get_rect(center=center)
+        if midtop is not None:
+            self.rect = self.surface.get_rect(midtop=midtop)
+        elif midbottom is not None:
+            self.rect = self.surface.get_rect(midbottom=midbottom)
 
     def move(self):
         self.rect.centerx -= 2
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
-from random import randint
+import random
+from math import floor
 from Pipe import Pipe
 
 
 def draw_floor():
     global floor_x
-    floor_x -= 1
+    floor_x -= 2
     screen.blit(floor_surface, (floor_x, screen_h - floor_h))
+
+
+def game_over():
+    pygame.quit()
 
 
 pygame.init()
@@ -20,14 +25,23 @@ screen = pygame.display.set_mode((screen_w, screen_h))
 # loading game images
 bg_surface = pygame.image.load('img/background-day.png').convert()
 floor_surface = pygame.image.load('img/floor.png').convert()
-bird_surface = pygame.image.load('img/bluebird-midflap.png').convert()
-pipe_surface = pygame.image.load('img/pipe-green.png').convert()
+bottom_pipe_surface = pygame.image.load('img/pipe-green.png').convert()
+top_pipe_surface = pygame.transform.rotate(bottom_pipe_surface, 180)
+# bird images
+bird_downflap = pygame.image.load('img/bluebird-downflap.png').convert()
+bird_midflap = pygame.image.load('img/bluebird-midflap.png').convert()
+bird_upflap = pygame.image.load('img/bluebird-upflap.png').convert()
+bird_frames = [bird_downflap, bird_midflap, bird_upflap]
 
 # game variables
-bird_rect = bird_surface.get_rect(center=(50, screen_h/2))
-bird_speed = 0
+game_active = True
+bird_rect = bird_upflap.get_rect(center=(50, screen_h/2))
+bird_yvelocity = 0
+bird_animation_counter = 0
+bird_animation_speed = 15
 gravity = 0.15
 pipes = []
+pipe_pos = [200, 275, 350]
 
 floor_x = 0
 floor_w = 336
@@ -42,29 +56,53 @@ while True:
             pygame.quit()
             break
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                bird_speed = -6
-        if event.type == SPAWNPIPE:
-            pipes.append(Pipe(pipe_surface, (screen_w/2, screen_h/2)))
+            if event.key == pygame.K_SPACE and game_active:
+                bird_yvelocity = -4
+            elif event.key == pygame.K_SPACE and not game_active:
+                game_active = True
+                bird_rect.center = (50, screen_h/2)
+                bird_yvelocity = 0
+                pipes.clear()
+        if event.type == SPAWNPIPE and game_active:
+            rdm_pipe_pos = random.choice(pipe_pos)
+            bottom_pipe = Pipe(bottom_pipe_surface, midtop=(500, rdm_pipe_pos))
+            top_pipe = Pipe(top_pipe_surface, midbottom=(500, rdm_pipe_pos - 150))
+            pipes.extend([bottom_pipe, top_pipe])
 
     screen.blit(bg_surface, (0, 0))
 
-    # bird
-    bird_speed += gravity
-    bird_rect.centery += bird_speed
+    if game_active:
+        # bird
+        bird_yvelocity += gravity
+        bird_rect.centery += bird_yvelocity
+        if bird_rect.top <= -50 or bird_rect.bottom >= screen_h - floor_h:
+            game_active = False
 
-    # pipes
-    for pipe in pipes:
-        pipe.move()
-        if pipe.rect.topright == 0:
-            pipes.remove(pipe)
-        pipe.draw(screen)
+        bird_index = floor(bird_animation_counter / bird_animation_speed)
+        if bird_index >= len(bird_frames) - 1:
+            bird_index = 0
+            bird_animation_counter = 0
+        bird_surface = bird_frames[bird_index]
+        bird_animation_counter += 1
 
+        rotated_bird = pygame.transform.rotate(bird_surface, -bird_yvelocity * 5)
+        screen.blit(rotated_bird, bird_rect)
+
+        # pipes
+        for pipe in pipes:
+            pipe.move()
+            if pipe.rect.right <= 0:
+                pipes.remove(pipe)
+            if pipe.rect.colliderect(bird_rect):
+                game_active = False
+                pipes.remove(pipe)
+            pipe.draw(screen)
+    else:
+        pass
     # floor
     draw_floor()
     if floor_x + floor_w < screen_w:
         floor_x = 0
 
-    screen.blit(bird_surface, bird_rect)
     pygame.display.update()
     clock.tick(120)
